e5b05cb8e32e821c2d96983908bcee8cd0ef8d3f
Robiul92 <robiullawal312@gmail.com>
2022-05-29 21:13:33 +0600

    Remove button added

--- src/Pages/Dashbord/Users.js
+++ src/Pages/Dashbord/Users.js
@@ -23,7 +23,7 @@ const Users = () => {
                             <th></th>
                             <th>Name</th>
                             <th>Job</th>
-                            <th>Favorite Color</th>
+                            <th>Romove User</th>
                         </tr>
                     </thead>
                     <tbody>
